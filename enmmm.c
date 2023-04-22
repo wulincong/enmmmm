3fdb6052f687e3e0d162d131007993b12a2337db
wulincong <wulincong@users.noreply.github.com>
2023-04-22 17:35:38 +0800

    enenenmmmm

---
+++ enmmm.c
@@ -0,0 +1,8 @@
+#include "enmmm.h"
+
+enmm enmmm enm mne enn
+    ennmm e;
+
+nne
+
+
---
+++ enmmm.h
@@ -0,0 +1,16 @@
+#define enmm int
+#define enmmm main
+#define enm (
+#define mne )
+#define enn {
+#define nne }
+#define ennmm return
+#define e 0
+
+
+
+
+
+
+
+
